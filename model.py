4c911928001c41df9472b63553531ec4a2ef052f
Sumeet Saini <sumeetsaini621@gmail.com>
2021-08-24 21:12:08 +0100

    adds creation of serialised model and coumn files

--- model.py
+++ model.py
@@ -3,6 +3,7 @@ import numpy as np
 import sklearn
 from sklearn import linear_model
 from sklearn.utils import shuffle
+import joblib
 
 data = pd.read_csv("student-mat.csv", sep=";")
 
@@ -18,21 +19,38 @@ predict = "G3"
 x = np.array(data.drop([predict], 1))
 y = np.array(data[predict])
 
+# Create test and train data
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
 
+# Train the model
 linear = linear_model.LinearRegression()
 linear.fit(x_train, y_train)
-accuracy = linear.score(x_test,y_test)
-print("Accuracy: ", accuracy)
 
-predictions = linear.predict(x_test)
+# Dump the model
+joblib.dump(linear, 'model.pkl')
+print("Model dumped!")
 
-for x in range(len(x_test)):
-    print(predictions[x], x_test[x], y_test[x])
+# Load the model
+linear = joblib.load('model.pkl')
 
+# Save the data columns
+model_columns = list(data.columns)
+joblib.dump(model_columns, 'model_columns.pkl')
+print("Models columns dumped!")
 
-sample = [[12, 13, 0, 0, 0]]
-predictions = linear.predict(sample)
-
-print(predictions, sample)
-
+# accuracy = linear.score(x_test,y_test)
+#
+#
+# print("Accuracy: ", accuracy)
+#
+# predictions = linear.predict(x_test)
+#
+# for x in range(len(x_test)):
+#     print(predictions[x], x_test[x], y_test[x])
+#
+#
+# sample = [[12, 13, 0, 0, 0]]
+# predictions = linear.predict(sample)
+#
+# print(predictions, sample)
+#
